cf2c711122287a0d40c2a6aac7369407814f8b81
nut <suppanat.nut@gmail.com>
2024-06-22 20:17:24 +0700

    Add : styling css-alert-button

---
+++ src/assets/checkcircle.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function CheckIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#14944F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M22 11.08V12a10 10 0 11-5.93-9.14"
+      ></path>
+      <path
+        stroke="#14944F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M22 4L12 14.01l-3-3"
+      ></path>
+    </svg>
+  );
+}
+
+export default CheckIcon;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,29 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function Button() {}
+function Button(props) {
+    let bgColor = props.color === "primary" ? "#074EE8" : props.color === "secondary" ? "#07A4E8" : undefined;
+    return (
+      <button
+        css={css`
+          background-color: ${bgColor};
+          margin: 10px;
+          width: 171.19px;
+          height: 50px;
+          border: none;
+          border-radius: 4px;
+          color: white;
+          font-family: 'Sarabun';
+          font-style: normal;
+          font-weight: 400;font-size: 16px;
+          line-height: 18px;
+        `}
+      >
+        Large
+      </button>
+    );
+  }
+
+
+export default Button
